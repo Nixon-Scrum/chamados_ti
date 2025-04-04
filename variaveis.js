de5f8de93870027d2180bd73d44d85a4bf3db669
Nixon Oliveira Marinho <nixon.devsistemas@gmail.com>
2025-04-04 01:41:37 -0400

    Trabalhando com Vetor e Objetos

---
+++ variaveis.js
@@ -0,0 +1,22 @@
+let nome = "Nixon"
+let sobreNome = "Oliveira Marinho"
+let idade = 43
+let altura = 1.73
+let peso = 98.35
+let casado = true
+let familia = [
+    {
+        nome : "Shéslyda",
+        parentesco : "Esposa"
+    }
+]
+
+// Concatenação
+
+console.log("Olá "+nome+" "+sobreNome+", você tem "+idade+" anos"+" e mede "+altura+"m e seu peso é "+peso+"kg.")
+
+// interpolação
+
+console.log(`Olá ${nome} ${sobreNome}, você tem ${idade} anos e mede ${altura}m e seu peso é ${peso}kg.`)
+
+console.log(familia[0])
